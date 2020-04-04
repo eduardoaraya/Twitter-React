bea31c4e79f6f4e87f16cbbeac5804a6856aacae
eduardoaraya <eduardojezine@gmail.com>
2020-04-04 14:20:38 -0300

    implement redux

--- src/components/PostText/PostText.js
+++ src/components/PostText/PostText.js
@@ -1,18 +1,25 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 
 import './style.css';
 
 export default function PostText() {
+
+  const tweetText = useRef(null);
+  
+  const tweetar = () => {
+    console.log(tweetText.current.value);
+  }
+
   return (
     <div className="post  border-highlight">
       <div className="post-area">
           <figure className="avatar"></figure>
           <div className="post-text">
-            <textarea className="post-input" placeholder="O que está acontecendo?"></textarea>
+            <textarea className="post-input" ref={tweetText} placeholder="O que está acontecendo?"></textarea>
           </div>
       </div>
       <div className="post-options">
-        <button className="btn btn-primary">Tweetar</button>
+        <button className="btn btn-primary" onClick={tweetar}>Tweetar</button>
       </div>
     </div>
   )
